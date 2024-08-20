dbd0a194464fe8c69d7d3938017945cc5d334c3a
Cláudio Roberto <claudioroberto0707@outlook.com>
2024-08-20 17:51:54 -0300

    informando os dados do banco

--- ESCOLA-CRUD/CONTROLES/controleEstudante.js
+++ ESCOLA-CRUD/CONTROLES/controleEstudante.js
@@ -1,10 +1,10 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-host: "localhost",
+host: "mysql.railway.internal",
 user: "root",
-password: "sua_senha",
-database: "Escola"
+password: "utTguYwrLrUCLwhscFoJQTsXgrDFilwK",
+database: "railway"
 });
 
 db.connect(err => {
